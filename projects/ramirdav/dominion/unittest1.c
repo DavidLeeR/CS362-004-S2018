629cf276e343aa9f48094a1a74241cd03c902206
Davidleer <davleeram91@gmail.com>
2018-04-27 19:45:57 -0600

    part 2 added to unit test 1

--- projects/ramirdav/dominion/unittest1.c
+++ projects/ramirdav/dominion/unittest1.c
@@ -14,10 +14,11 @@
 #include "rngs.h"
 
 //Unit Test for isGameOver() function
-//1. isGameOver() should return 1 (game over) if there are 0 province cards left
-//2. isGameOver() should return 1 (game over) if there are 3 empty supply piles
-//3. isGameOver() should return 1 (game over) if there are over 3 supply piles
-//3. isGameOver() should return 0 (game continues) if there are 2 empty supply piles
+//1. isGameOver() should return 0 (continue game) if called directly after initializing game (ie. when supply cards and province card are full)
+//2. isGameOver() should return 1 (game over) if there are 0 province cards left
+//3. isGameOver() should return 1 (game over) if there are 3 empty supply piles
+//4. isGameOver() should return 1 (game over) if there are over 3 supply piles
+//5. isGameOver() should return 0 (game continues) if there are 2 empty supply piles
 int main() 
 {
     int seed = 1000;
@@ -28,12 +29,23 @@ int main()
     struct gameState G;
 
     initCheck = initializeGame(playerCount, k, seed, &G);
-    assert(initCheck == 0);
 
-    tester = isGameOver(&G);     
+    //part 1.
+    tester = isGameOver(&G);
+    printf("Unit Test 1, part 1...  expected result: isGameOver() == 0 , actual result: isGameOver() == %d\n", tester);     
     assert(tester == 0);            //isGameOver() should return 0 to show that the game should continue
 
-   // G->supplyCount[province] = 0;
-   return 0;
+
+    //part 2.
+    G->supplyCount[province] = 0;
+
+    tester = isGameOver(&G);
+    printf("Unit Test 1, part 2...  expected result: isGameOver() == 1 , actual result: isGameOver == %d\n", tester);  
+    assert(tester == 1)             //isGameOver() should return 1 to show that the game should end
+
+    //part 3.
+    
+
+    return 0;
 
 }
