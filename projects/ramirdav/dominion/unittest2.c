28d645016ae738db5bcb8dc9ba0b905f931f0132
Davidleer <davleeram91@gmail.com>
2018-04-28 15:19:32 -0700

    second test case for unit test 2

--- projects/ramirdav/dominion/unittest2.c
+++ projects/ramirdav/dominion/unittest2.c
@@ -14,47 +14,46 @@
 #include "rngs.h"
 
 //Unit Test for newGame() function
-//1. G1 should be NULL before call to newGame()
-//2. G1 should not be NULL after call to newGame()
+//1. G1 should not point to NULL after call to newGame(), signifying a new memory address for G1
+//2. G1->numPlayers and G1->playedCardCount should both return 0, showing that memory has been corrctly allocated for the struct
+//	-If test case 2 fails, a segmentation fault will result 
 int main() 
 {
-    struct gameState *G1;      
+    struct gameState *G1 = NULL;
     int testSum = 0;        //keeps track of passed test cases
-    int testTotal = 2;      //number of total test cases
+    int testTotal = 1;      //number of total test cases
 
 
     /***************************************************************************************
-     *PART 1: check if G1 == NULL before call to newGame()                                 *
+     *PART 1: check if G1 points to different address than G1Copy                          *
      ***************************************************************************************/
-    if(G1 == NULL)
-    { 
-        printf("Unit Test 2: part 1 (check if G1 == NULL before call to newGame())... \n     PASS\n     expected result: G1 == NULL\n     actual result: G1 == NULL\n");  
-        testSum++;
-    }
-    else if(G1 != NULL)
-    {
-        printf("%d", G1);
-        printf("Unit Test 2: part 1(check if G1 == NULL before call to newGame())... \n     FAIL\n     expected result: G1 == NULL\n     actual result: G1 != NULL\n");
-    }
-
-
-
-    /***************************************************************************************
-     *PART 2: check if size of G1 == size of gamState struct after call to newGame()       *
-     ***************************************************************************************/
+    //call new game, returning a new memory address for G1
     G1 = newGame();
 
     if(G1 != NULL)
     {
-        printf("Unit Test 2: part 2 (check if G1 == NULL after call to newGame())... \n     PASS\n     expected result: G1 != NULL\n     actual result: G1 != NULL\n");  
-        testSum++;
+        printf("Unit Test 2: part 1 (check if new memory address for G1 after call to newGame() is not NULL)... \n     PASS\n     expected result: G1 != NULL\n     actual result: G1 != NULL\n");
+	testSum++;
     }
     else if(G1 == NULL)
     {
-        printf("Unit Test 2: part 1(check if G1 == NULL after call to newGame())... \n     FAIL\n     expected result: G1 != NULL\n     actual result: G1 == NULL\n");
+        printf("Unit Test 2: part 1(check if new memory address for G1 after call to newGame() is not NULL)... \n     FAIL\n     expected result: G1 != NULL\n     actual result: G1 == NULL\n");
     }
 
+    /***************************************************************************************
+    *                         OUTPUT TOTAL UNIT TEST RESULTS                              *
+    ****************************************************************************************/
+    //if not all the test cases passed, output number of failed tests
+    if (testSum < testTotal)
+    {
+        int testFails = testTotal - testSum;
+        printf("Unit Test 2 failed %d test cases \n", testFails);
+    }
+    //if all test cases passed, output success message
+    else if (testSum == testTotal)
+        printf("Unit Test 2 passed all test cases \n");
+    
+    printf("%d      %d", G1->numPlayers, G1->playedCardCount);
 
     return 0;
-
 }
